c4c201a55aea46cccfe7db9fbb6b69d8e94323ec
onelong <love@onelong.org>
2018-07-17 11:38:25 +0800

    fix(page): 删除x-image展示页代码多余的空格

--- src/pages/x-image.vue
+++ src/pages/x-image.vue
@@ -9,7 +9,7 @@
 
 <template lang="pug">
 view 懒加载100张图片，可以通过控制台看到相关变化
- x-image(wx:for="{{100}}" class="logo" wx:key="{{index}}" src="http://s1.51cto.com/wyfs02/M02/8A/A1/wKiom1g1d4KzJLj_AAENEPluz5E910.jpg-wh_651x-s_1750868483.jpg" loadHolder="/imgs/weapp.jpeg" lazy-load="{{true}}" bind:error="_error" bind:load="_load")
+x-image(wx:for="{{100}}" class="logo" wx:key="{{index}}" src="http://s1.51cto.com/wyfs02/M02/8A/A1/wKiom1g1d4KzJLj_AAENEPluz5E910.jpg-wh_651x-s_1750868483.jpg" loadHolder="/imgs/weapp.jpeg" lazy-load="{{true}}" bind:error="_error" bind:load="_load")
 </template>
 
 <script>
